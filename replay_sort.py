5dda67dda3bda7164b1437e3e4bce7452f538873
Kyle Golden <kg4332@outlook.com>
2014-05-02 11:29:52 -0400

    Create replay_sort.py

---
+++ replay_sort.py
@@ -0,0 +1,22 @@
+import os
+import glob
+
+"""
+AOE3 Replay Sort
+
+Age of Empires 3 replays are saved to generic default names (age3rec1.rec) which make it difficult
+to find replays.
+
+The idea of this program is to scrape these replay files for information such as player names/civs and hopefully
+create a new file with a more descriptive name
+
+currently a WIP
+
+"""
+
+def run():
+
+def get_rec():
+
+if __name__ == '__main__':
+  run()
